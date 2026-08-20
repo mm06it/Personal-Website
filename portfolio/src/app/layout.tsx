474f799047c6fcbba91545a77dc4482eea9b7c05
mm06it <mmayr006@gmail.com>
2026-08-20 22:43:39 +0200

    Update layout.tsx

--- portfolio/src/app/layout.tsx
+++ portfolio/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next";
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
 import "./globals.css";
